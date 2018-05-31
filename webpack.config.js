06057010ec7f5a86710f1026e3005a1c758b035b
Emil Norén <emil.noren@wiseweb.se>
2018-05-31 14:39:22 +0200

    Got sourcemaps for scss working

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const Path = require('path'),
-      CircularDependencyPlugin = require('circular-dependency-plugin'),
-      ExtractTextPlugin = require('extract-text-webpack-plugin'),
-      WebpackNotifierPlugin = require('webpack-notifier');
+			CircularDependencyPlugin = require('circular-dependency-plugin'),
+			ExtractTextPlugin = require('extract-text-webpack-plugin'),
+			WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-  // mode: 'development',
-  mode: 'production',
-  devtool: 'source-map',
-  entry: ['./src/app.js', './src/scss/main.scss'],
-  output: {
-    path: Path.resolve(__dirname, 'dist'),
-    filename: './assets/js/main.bundle.js'
-  },
-  module: {
-    rules: [
+	// mode: 'development',
+	mode: 'production',
+	devtool: 'source-map',
+	entry: ['./src/app.js', './src/scss/main.scss'],
+	output: {
+		path: Path.resolve(__dirname, 'dist'),
+		filename: './assets/js/main.bundle.js'
+	},
+	module: {
+		rules: [
 
-      // Babel-loader for webpack
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins:
+			// Babel-loader for webpack
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins:
 						[
 							[
 								'@babel/plugin-transform-runtime',
@@ -33,50 +33,65 @@ module.exports = {
 								}
 							],
 						],
-          }
-        }
-      },
+					}
+				}
+			},
 
-      // Regular CSS files
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader?importLoaders=1',
-        }),
-      },
+			// Regular CSS files
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					use: 'css-loader?importLoaders=1',
+				}),
+			},
 
-      // SCSS loader for webpack
-      {
-        test: /\.(sass|scss)$/,
-        use: ExtractTextPlugin.extract([
-          'css-loader', 
-          'postcss-loader', 
-          'sass-loader'
-        ])
-      }
+			// SCSS loader for webpack
+			{
+				test: /\.(sass|scss)$/,
+				use: ExtractTextPlugin.extract([
+					{
+						loader: 'css-loader', 
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'postcss-loader', 
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader', 
+						options: {
+							sourceMap: true
+						}
+					}
+				])
+			}
 
-    ]
-  },
-  plugins: [
+		]
+	},
+	plugins: [
 
-    // Get rid of circular dependencies
-    new CircularDependencyPlugin({
-      exclude: /a\.js|node_modules/,
-      failOnError: true,
-      allowAsyncCycles: false,
-      cwd: process.cwd(),
-    }),
+		// Get rid of circular dependencies
+		new CircularDependencyPlugin({
+			exclude: /a\.js|node_modules/,
+			failOnError: true,
+			allowAsyncCycles: false,
+			cwd: process.cwd(),
+		}),
 
-    // Send a notification when the build is finished
-    new WebpackNotifierPlugin({
-      title: 'Webpack'
-    }),
+		// Send a notification when the build is finished
+		new WebpackNotifierPlugin({
+			title: 'Webpack'
+		}),
 
-    // Define where to save the file
-    new ExtractTextPlugin({
-      filename: './assets/css/[name].bundle.css',
-      allChunks: true,
-    }),
+		// Define where to save the file
+		new ExtractTextPlugin({
+			filename: './assets/css/[name].bundle.css',
+			allChunks: true,
+		}),
 
-  ],
+	],
 };
