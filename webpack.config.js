7b4bcdb2033c338637ade6dfce222d1686646a4d
Emil Norén <emil.noren@wiseweb.se>
2018-06-08 11:03:42 +0200

    Reworked project to work with node

--- webpack.config.js
+++ webpack.config.js
@@ -3,95 +3,164 @@ const Path = require('path'),
 			ExtractTextPlugin = require('extract-text-webpack-plugin'),
 			WebpackNotifierPlugin = require('webpack-notifier');
 
-module.exports = {
-	// mode: 'development',
-	mode: 'production',
-	devtool: 'source-map',
-	entry: ['./src/app.js', './src/scss/main.scss'],
-	output: {
-		path: Path.resolve(__dirname, 'dist'),
-		filename: './assets/js/main.bundle.js'
-	},
-	module: {
-		rules: [
+// Our config constants
 
-			// Babel-loader for webpack
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins:
-						[
+module.exports = [
+
+	/* CLIENTSIDE CONFIG */
+	{
+		// mode: 'development',
+		mode: 'production',
+		devtool: 'source-map',
+		entry: ['./src/client/main.js', './src/client/scss/main.scss'],
+		output: {
+			path: Path.resolve(__dirname, 'dist'),
+			filename: './assets/js/main.bundle.js'
+		},
+		module: {
+			rules: [
+
+				// Babel-loader for webpack
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins:
 							[
-								'@babel/plugin-transform-runtime',
-								{
-									polyfill    : false,
-									regenerator : true,
-								}
+								[
+									'@babel/plugin-transform-runtime',
+									{
+										polyfill    : false,
+										regenerator : true,
+									}
+								],
 							],
-						],
+						}
 					}
+				},
+
+				// Regular CSS files
+				{
+					test: /\.css$/,
+					use: ExtractTextPlugin.extract({
+						use: 'css-loader?importLoaders=1',
+					}),
+				},
+
+				// SCSS loader for webpack
+				{
+					test: /\.(sass|scss)$/,
+					use: ExtractTextPlugin.extract([
+						{
+							loader: 'css-loader', 
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'postcss-loader', 
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader', 
+							options: {
+								sourceMap: true
+							}
+						}
+					])
 				}
-			},
 
-			// Regular CSS files
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					use: 'css-loader?importLoaders=1',
-				}),
-			},
+			]
+		},
+		plugins: [
 
-			// SCSS loader for webpack
-			{
-				test: /\.(sass|scss)$/,
-				use: ExtractTextPlugin.extract([
-					{
-						loader: 'css-loader', 
+			// Get rid of circular dependencies
+			new CircularDependencyPlugin({
+				exclude: /a\.js|node_modules/,
+				failOnError: true,
+				allowAsyncCycles: false,
+				cwd: process.cwd(),
+			}),
+
+			// Send a notification when the build is finished
+			new WebpackNotifierPlugin({
+				title: 'Client assets'
+			}),
+
+			// Define where to save the compiled css file
+			new ExtractTextPlugin({
+				filename: './assets/css/[name].bundle.css',
+				allChunks: true,
+			}),
+
+		],
+	},
+
+	/* SERVER CONFIG */
+	{
+		mode: 'development',
+		target: 'node',
+		devtool: 'source-map',
+		entry: ['./src/server/app.js'],
+		node: {
+			fs: 'empty',
+			net: 'empty'
+		},
+		output: {
+			path: Path.resolve(__dirname, 'dist'),
+			libraryTarget: 'commonjs',
+			filename: './app.js'
+		},
+		externals: [
+			/^(?!\.|\/).+/i,
+		],
+		module: {
+			rules: [
+
+				// Babel-loader for webpack
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					use: {
+						loader: 'babel-loader',
 						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'postcss-loader', 
-						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'sass-loader', 
-						options: {
-							sourceMap: true
+							presets: ['@babel/preset-env'],
+							plugins:
+							[
+								[
+									'@babel/plugin-transform-runtime',
+									{
+										polyfill    : false,
+										regenerator : true,
+									}
+								],
+							],
 						}
 					}
-				])
-			}
+				},
 
-		]
-	},
-	plugins: [
+			]
+		},
+		plugins: [
 
-		// Get rid of circular dependencies
-		new CircularDependencyPlugin({
-			exclude: /a\.js|node_modules/,
-			failOnError: true,
-			allowAsyncCycles: false,
-			cwd: process.cwd(),
-		}),
+			// Get rid of circular dependencies
+			new CircularDependencyPlugin({
+				exclude: /a\.js|node_modules/,
+				failOnError: true,
+				allowAsyncCycles: false,
+				cwd: process.cwd(),
+			}),
 
-		// Send a notification when the build is finished
-		new WebpackNotifierPlugin({
-			title: 'Webpack'
-		}),
+			// Send a notification when the build is finished
+			new WebpackNotifierPlugin({
+				title: 'Server assets'
+			}),
 
-		// Define where to save the compiled css file
-		new ExtractTextPlugin({
-			filename: './assets/css/[name].bundle.css',
-			allChunks: true,
-		}),
-
-	],
-};
+		],
+	}
+];
